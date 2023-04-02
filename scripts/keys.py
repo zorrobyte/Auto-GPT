f24513cbee04a18d701c842290e3c4ad4ce02539
Ross Fisher <zorrobyte@gmail.com>
2023-04-02 17:53:33 -0400

    sddsf

--- scripts/keys.py
+++ scripts/keys.py
@@ -1,5 +1,5 @@
 # Get yours from:  https://beta.openai.com/account/api-keys
-OPENAI_API_KEY = "YOUR-OPENAI-KEY"
+OPENAI_API_KEY = "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"
 # To access your ElevenLabs API key, head to https://elevenlabs.io, you
 # can view your xi-api-key using the 'Profile' tab on the website.
 ELEVENLABS_API_KEY = "YOUR-ELEVENLABS-KEY"
